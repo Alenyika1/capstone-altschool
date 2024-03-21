1458033035026d462b2468da8c32d6933a0a97ea
Jesse Alenyika <alenyikaj@gmail.com>
2024-03-21 04:29:37 +0100

    New files updatd

--- modules/aks/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "cluster_name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "dns_prefix" {}
-variable "node_count" {}
-variable "vm_size" {}
-variable "environment" {}
---
+++ terraform/0-local.tf
@@ -0,0 +1,7 @@
+locals {
+  env                 = "dev"
+  region              = "eastus2"
+  resource_group_name = "capstone"
+  eks_name            = "capstone-eks"
+  eks_version         = "1.27"
+}
